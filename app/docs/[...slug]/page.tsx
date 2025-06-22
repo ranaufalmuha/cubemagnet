d71c7e0c6a98e733e59a2f2936d448aada3f97ec
ranaufalmuha <ifal.muha@gmail.com>
2025-06-22 09:55:51 +0700

    update code for slug

--- app/docs/[...slug]/page.tsx
+++ app/docs/[...slug]/page.tsx
@@ -1,17 +1,25 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { getDocBySlug } from "@/lib/content";
 import rehypePrism from "rehype-prism-plus";
-import { PageParams } from "@/types/route";
 
-export default function DocPage({ params }: PageParams) {
-  const { data, content } = getDocBySlug(params.slug.join("/"));
+export default async function DocPage({
+  params,
+}: {
+  params: { slug: string | string[] };
+}) {
+  const slugArray = Array.isArray(params.slug) ? params.slug : [params.slug];
+  const path = slugArray.join("/");
+
+  const { data, content } = await getDocBySlug(path); // if getDocBySlug is async
 
   return (
     <article className="prose p-8">
       <h1>{data.title}</h1>
       <MDXRemote
         source={content}
-        options={{ mdxOptions: { rehypePlugins: [rehypePrism] } }}
+        options={{
+          mdxOptions: { rehypePlugins: [rehypePrism] },
+        }}
       />
     </article>
   );
